8a3307b595f0846def2be1ee61b9486403beffb2
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2024-12-14 17:28:42 +0100

    debug in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 // BACKEND_URL for the dev system
-export const BACKEND_URL = 'http://127.0.0.1';
+// export const BACKEND_URL = 'http://127.0.0.1';
 
 // BACKEND_URL for the production system
-// export const BACKEND_URL = 'http://api.immportant.com';
+export const BACKEND_URL = 'https://api.immportant.com';
